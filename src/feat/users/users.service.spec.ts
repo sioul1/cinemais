c45303336777521f7646028e4c83a1a30ed57d7f
werbm <werbm.silva.grupowra@outlook.com>
2025-09-04 01:18:57 -0300

    fix: badRequestException at testing addFavorite

--- src/feat/users/users.service.spec.ts
+++ src/feat/users/users.service.spec.ts
@@ -1,20 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import { NotFoundException } from '@nestjs/common';
+import { NotFoundException, BadRequestException } from '@nestjs/common';
 import { DatabaseService } from '../../core/database/database.service';
 
 const prismaMock = {
   user: {
     findUnique: jest.fn().mockResolvedValue({ id: 'user-1' }),
-    create: jest.fn().mockResolvedValue({ id: 'user-1' }),
-    upsert: jest.fn().mockResolvedValue({ id: 'user-1' }),
   },
   media: {
     findUnique: jest.fn().mockResolvedValue({ id: 'media-1' }),
   },
   favorite: {
     create: jest.fn(),
-    findUnique: jest.fn().mockResolvedValue({ id: 'fav-1' }),
+    findUnique: jest.fn(),
     delete: jest.fn(),
   },
 };
@@ -45,12 +43,16 @@ describe('UsersService', () => {
   });
 
   describe('addFavorite', () => {
-    it('deve adicionar uma mídia aos favoritos de um usuário', async () => {
+    it('deve adicionar uma mídia aos favoritos de um usuário se não existir', async () => {
       const addFavoriteDto = { mediaId: 'media-1' };
+      prismaMock.favorite.findUnique.mockResolvedValueOnce(null);
+
       await service.addFavorite('user-1', addFavoriteDto);
+
       expect(prismaMock.favorite.create).toHaveBeenCalledWith({
         data: { userId: 'user-1', mediaId: 'media-1' },
       });
+      expect(prismaMock.favorite.findUnique).toHaveBeenCalled();
     });
 
     it('deve lançar um NotFoundException se a mídia não existir', async () => {
@@ -60,10 +62,24 @@ describe('UsersService', () => {
         service.addFavorite('user-1', addFavoriteDto),
       ).rejects.toThrow(NotFoundException);
     });
+
+    it('deve lançar um BadRequestException se a mídia já for um favorito', async () => {
+      const addFavoriteDto = { mediaId: 'media-1' };
+      prismaMock.favorite.findUnique.mockResolvedValueOnce({
+        id: 'fav-1',
+        userId: 'user-1',
+        mediaId: 'media-1',
+      });
+      await expect(
+        service.addFavorite('user-1', addFavoriteDto),
+      ).rejects.toThrow(BadRequestException);
+      expect(prismaMock.favorite.create).not.toHaveBeenCalled();
+    });
   });
 
   describe('removeFavorite', () => {
     it('deve remover um favorito com sucesso', async () => {
+      prismaMock.favorite.findUnique.mockResolvedValueOnce({ id: 'fav-1' });
       await service.removeFavorite('user-1', 'media-1');
       expect(prismaMock.favorite.delete).toHaveBeenCalledWith({
         where: { id: 'fav-1' },
